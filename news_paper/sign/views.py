ea97db1831f7d86f0cf398458e9f495a9480a102
Katya <vasineva.1994@gmail.com>
2025-02-01 22:12:17 +1000

    2

---
+++ news_paper/sign/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import Group, User
+from django.views.generic.edit import CreateView
+from .models import BaseRegisterForm, ProfileUpdateForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'sign/profile_form.html'
+    form_class = ProfileUpdateForm
+    success_url = '/'
+
+    def get_object(self):
+        return self.request.user
+
+
+
+class BaseRegisterView(CreateView):
+    model = User
+    form_class = BaseRegisterForm
+    success_url = '/'
+
+@login_required
+def user_profile(request):
+    return render(request, 'index.html', {'user': request.user})
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    premium_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        premium_group.user_set.add(user)
+    return redirect('/')
